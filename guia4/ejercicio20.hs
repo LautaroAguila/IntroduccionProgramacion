a7d05ec7700258a5d9fa47f188663aa1a0fcb40c
LautaroAguilar <lautaroaguilar.c@gmail.com>
2024-04-21 22:43:42 -0300

    guia 4 21/04

--- guia4/ejercicio20.hs
+++ guia4/ejercicio20.hs
@@ -1,2 +1,10 @@
-tomaValorMax :: Int ->Int ->Int
-tomaValorMax n n = m
+tomaValorMax :: Int -> Int ->Int
+tomaValorMax x y    | x == y = x
+                    | otherwise = div y (menorDivisor y)
+menorDivisor :: Int -> Int
+menorDivisor n = buscarDivisor n 2
+
+buscarDivisor :: Int -> Int -> Int
+buscarDivisor n k   | k * k > n = n  
+                    | mod n k == 0 = k 
+                    | otherwise = buscarDivisor n (k + 1)
